4588a6a6fbbfd7dc5c71303a0240e5b69d44e9e9
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-01-10 21:22:34 +0600

    Profile avatar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -47,8 +47,8 @@ const Navbar = () => {
                 </li>
                 <img
                     className="size-10 rounded-full bg-slate-500 object-cover"
-                    src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop"
-                    alt="avatar navigate ui"
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                    alt="profile picture"
                 />
             </ul>
         </div>
